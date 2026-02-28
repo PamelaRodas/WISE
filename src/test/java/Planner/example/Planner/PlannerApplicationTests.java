8b8727348a08c87c69000e450f9b8203610d9369
PamelaRodqas <pameci09@hotmail.com>
2026-02-28 18:06:12 -0500

    chore: initial commit before java upgrade

---
+++ src/test/java/Planner/example/Planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package Planner.example.Planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
